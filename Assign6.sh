8f9187e1998a90951d9f56b0fa740babd6976137
SanketGawande27 <53644173+SanketGawande27@users.noreply.github.com>
2020-08-17 22:25:00 +0530

    sixth assign

---
+++ Assign6.sh
@@ -0,0 +1,19 @@
+
+read -p "Enter Day :" day
+
+case "${day}" in
+   "monday" )
+     echo "Working Day ";;
+   "tuesday" )
+      echo "Working Day ";;
+   "wednesday" )
+      echo "Working Day " ;;
+   "thursday" )
+      echo "Working Day " ;;
+   "friday" )
+      echo "Working Day " ;;
+   "saturday" )
+     echo "Weakend ....." ;;
+   "sunday" )
+      echo "Weakend....." ;;
+esac
